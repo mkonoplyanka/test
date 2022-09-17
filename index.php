f96c130e791af27e1d029bdde44632df4cbb4e5c
mihailkonoplanka <mkonoplyanka@gmail.com>
2022-09-18 00:50:59 +0300

    setters getters

--- index.php
+++ index.php
@@ -1,2 +1,45 @@
 <?php
-echo 'Test 2';
+
+class Colors
+{
+    private $red;
+    private $green;
+    private $blue;
+
+    public function __construct($red, $green, $blue)
+    {
+        $this->setRed($red);
+        $this->setGreen($green);
+        $this->setBlue($blue);
+    }
+
+    public function getRed()
+    {
+        return $this->red;
+    }
+
+    public function setRed($red)
+    {
+        $this->red = $red;
+    }
+
+    public function getGreen()
+    {
+        return $this->green;
+    }
+
+    public function setGreen($green)
+    {
+        $this->green = $green;
+    }
+
+    public function getBlue()
+    {
+        return $this->blue;
+    }
+
+    public function setBlue($blue)
+    {
+        $this->blue = $blue;
+    }
+}

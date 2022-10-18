c27d2f641f02f01824279294de8facb04aa5800a
mihailkonoplanka <mkonoplyanka@gmail.com>
2022-10-18 14:33:51 +0300

    HW7 Done

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -7,23 +7,31 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
 </head>
 <body>
+
 <div class="container">
     <div class="row">
-        <ul>
-            <li>
-                <a href="/public/categories/list-categories.php">List of categories</a>
-            </li>
-            <li>
-                <a href="/public/tags/list-tags.php">List of tags</a>
-            </li>
-        </ul>
+        <div class="col">
+            <ul>
+                <li>
+                    <a href="/public/categories/list-categories.php">List of categories</a>
+                </li>
+                <li>
+                    <a href="/public/tags/list-tags.php">List of tags</a>
+                </li>
+            </ul>
+        </div>
     </div>
 </div>
+
 <div class="container">
     <div class="row">
-        @yield('content')
+        <div class="col">
+            @yield('content')
+        </div>
     </div>
 </div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
+
 </body>
 </html>
